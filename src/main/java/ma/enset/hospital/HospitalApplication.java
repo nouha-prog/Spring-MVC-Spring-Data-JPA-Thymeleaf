2da552bb2f6edc2ec84818c456fae5b772f8b7bd
nouhaila <nouhaila@exemple.com>
2025-03-24 10:28:50 +0000

    modification Main

--- src/main/java/ma/enset/hospital/HospitalApplication.java
+++ src/main/java/ma/enset/hospital/HospitalApplication.java
@@ -1,13 +1,35 @@
 package ma.enset.hospital;
 
+import ma.enset.hospital.entities.Patient;
+import ma.enset.hospital.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-public class HospitalApplication {
+import java.util.Date;
+
+
+@SpringBootApplication(scanBasePackages = "ma.enset.hospital")
+@EnableJpaRepositories("ma.enset.hospital.repository")
+@EntityScan("ma.enset.hospital.entities")
+public class HospitalApplication implements CommandLineRunner {
+
+    @Autowired
+    private PatientRepository patientRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(HospitalApplication.class, args);
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        patientRepository.save(new Patient(null, "Mohammed", new Date(), false, 40));
+        patientRepository.save(new Patient(null, "Hanane", new Date(), false, 334));
+        patientRepository.save(new Patient(null, "Imane", new Date(), true, 304));
     }
 
 }
