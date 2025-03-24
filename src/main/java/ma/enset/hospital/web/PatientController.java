c96cdc0949d4ac336fe50cc9ca1ed039b8da6d8f
nouhaila <nouhaila@exemple.com>
2025-03-24 10:56:34 +0000

    modification du code

--- src/main/java/ma/enset/hospital/web/PatientController.java
+++ src/main/java/ma/enset/hospital/web/PatientController.java
@@ -1,19 +1,55 @@
 package ma.enset.hospital.web;
-import lombok.AllArgsConstructor;
-import ma.enset.hospital.repository.PatientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.AllArgsConstructor;
+import ma.enset.hospital.entities.Patient;
+import ma.enset.hospital.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
 public class PatientController {
-
     private PatientRepository patientRepository;
     @GetMapping("/index")
-
-    public String index(){
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "0") int p,
+                        @RequestParam(name = "size", defaultValue = "5") int s,
+                        @RequestParam(name = "keyword", defaultValue = "") String kw){
+        Page<Patient> patientPage = patientRepository.findByNomContains(kw, PageRequest.of(p, s));
+        model.addAttribute("listPatients", patientPage.getContent());
+        model.addAttribute("pages", new int[patientPage.getTotalPages()]);
+        model.addAttribute("currentPage", p);
+        model.addAttribute("keyword", kw);
         return "patients";
+    }
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/index";
+    }
+    @GetMapping("/patients")
+    @ResponseBody
+    public List<Patient> patients(){
+        return patientRepository.findAll();
+    }
+    @GetMapping("/delete")
+    public String delete(Long id, String keyword, int page){
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+    @GetMapping( "/editPatient")
+    public String editPatient(Model model, Long id, String keyword, int page){
+        Patient patient = patientRepository.findById(id).orElse(null);
+        if (patient == null) throw new RuntimeException("Patient not found");
+        model.addAttribute("patient", patient);
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("page", page);
+        return "editPatient";
     }
 }
